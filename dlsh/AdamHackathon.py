1a46075d41092262003a2cc7177c99eb256bc5d0
Adam Urban <99017970+adamurban04@users.noreply.github.com>
2024-05-01 13:27:30 +0100

    Update AdamHackathon.py

--- dlsh/AdamHackathon.py
+++ dlsh/AdamHackathon.py
@@ -1,4 +1,5 @@
 import random
+from collections import deque
 
  # Main function (not necessary but we use it for the code flow)
 
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     
     for line in lines:
         ID, last_name, first_name = line.strip().split(',')
-        students_dictionary[last_name] = {'ID': ID, 'first_name': first_name}
+        students_dictionary[last_name] = {'ID': ID, 'last_name': last_name, 'first_name': first_name}
 
 
 # Sort the array (Last Name) (Insertion Sort)
@@ -36,17 +37,11 @@ if __name__ == "__main__":
     # Get the list of last names and sort it
     last_names = list(students_dictionary.keys())
     insertionSort(last_names)
-    print(last_names)
 
     # Update the dictionary with sorted last names
     sorted_students_dictionary = {}
     for last_name in last_names:
         sorted_students_dictionary[last_name] = students_dictionary[last_name]
-
-    # Print the sorted dictionary
-    print(sorted_students_dictionary)
-    for last_name, student_info in sorted_students_dictionary.items():
-        print(f"Last Name: {last_name}, First Name: {student_info['first_name']}, ID: {student_info['ID']}")
 
     
    # THE DICTIONARY IS NOW SORTED BY THE LAST NAME
@@ -55,19 +50,34 @@ if __name__ == "__main__":
     # Open the file for writing
     with open('cs4222_students_list.csv', 'w') as file: # deletes everything just by opening in 'w', so watch out when running the code more than once
 
-    # Iterate over the dictionary
-    for ID, info in sorted_students_dictionary.items():
-        last_name = info['last_name']
-        first_name = info['first_name']
-        module_code1 = 'CS4221'
-        module_code2 = 'CS4222'
-        module_code3 = 'CS4141'
-        module_title1 = 'Foundation of Computer Science 1'
-        module_title2 = 'Software Development'
-        module_title3 = 'Introducton to Programming'
+        # Iterate over the dictionary
+        for ID, info in sorted_students_dictionary.items():
+            last_name = info['last_name']  
+            first_name = info['first_name'] 
+            module_code1 = 'CS4221'
+            module_code2 = 'CS4222'
+            module_code3 = 'CS4141'
+            module_title1 = 'Foundation of Computer Science 1'
+            module_title2 = 'Software Development'
+            module_title3 = 'Introduction to Programming'
 
-        marks1 = random.randint(0, 100)
-        marks2 = random.randint(0, 100) 
-        marks3 = random.randint(0, 100) 
+            marks1 = random.randint(0, 100)
+            marks2 = random.randint(0, 100) 
+            marks3 = random.randint(0, 100) 
 
-        file.write(f"{ID},{last_name},{first_name},{module_code1},{module_title1},{marks1}, {module_code2},{module_title2},{marks2}, {module_code3},{module_title3},{marks3}\n")
+            file.write(f"{ID},{last_name},{first_name},{module_code1},{module_title1},{marks1}, {module_code2},{module_title2},{marks2}, {module_code3},{module_title3},{marks3}\n")
+
+
+    marks1_list = [int(info['marks1']) for info in sorted_students_dictionary.values()]
+
+    # Sort the marks1 list
+    insertionSort(marks1_list)
+
+    i = len(marks1_list-5)
+    top_markers1 = []
+    while i < len(marks1_list):
+        # stack
+        top_markers1.append(marks1_list[i])
+        i += 1
+
+    print(top_markers1)
